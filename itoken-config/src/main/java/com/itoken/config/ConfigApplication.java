00c9a03182c56da7c7e86ba74853f1cfd57b78b9
tangjian <1059020231@qq.com>
2021-04-24 18:57:53 +0800

    update

--- itoken-config/src/main/java/com/itoken/config/ConfigApplication.java
+++ itoken-config/src/main/java/com/itoken/config/ConfigApplication.java
@@ -1,12 +1,13 @@
-package com.itoken.eureka;
-
+package com.itoken.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.security.auth.login.Configuration;
+
 @SpringBootApplication
-public class EurekaApplication {
+public class ConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class,args);
+        SpringApplication.run(ConfigApplication.class,args);
     }
 }
